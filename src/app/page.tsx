33d5b811d5bc4739a8fa9fd294211cdafb263bc3
AleksySzlapa <szlapaaleksy@gmail.com>
2025-10-15 03:57:36 +0200

    chore: init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+import Sidebar from "@/components/layout/Sidebar";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen overflow-hidden">
+    </main>
+  );
+}
